07eaced0ed20cb06fd6c01efb96a40beb35481e1
edwin.gonzalez24 <edgoca2498@gmail.com>
2020-03-09 09:15:41 -0600

    Added data views

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const Viaje = require('../models/Viajes')
+
 module.exports = function() {
     // USE -> Acepta todos los verbos de http
     // GET -> Acepta solo GET 
     // Render / Send
+
+    // Routing  
+    //Definiendo las vistas 
     // Render + Name File views
     router.get('/', (req, res) => {
         res.render('index', {
@@ -12,11 +17,19 @@ module.exports = function() {
         })
     })
 
-    // Routing
     router.get('/nosotros', (req, res) => {
         res.render('nosotros', {
             pagina: 'Nosotros'
         });
+    });
+
+    router.get('/viajes', (req, res) => {
+        Viaje.findAll()
+            .then(viajes => res.render('viajes', {
+                pagina: 'Proximos Viajes',
+                viajes
+            }))
+            .catch(error => console.log(error))
     });
 
     return router;
